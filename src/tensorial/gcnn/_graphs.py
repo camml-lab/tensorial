Fixed problem with metrics normalization
The metrics were not dividing by the number of elements in a tensor
(only the number of tensors). This is typically not what you want,
because the metric should be on the same 'scale' as the numbers as in
the tensor.

src/tensorial/gcnn/_graphs.py
@@ -9,9 +9,10 @@ import jax.numpy as jnp
 import jaxtyping as jt
 import jraph
 import numpy as np
+import reax.metrics
 
 import tensorial
-from tensorial import geometry, nn_utils, typing
+from tensorial import geometry, typing
 
 from . import keys
 
@@ -147,8 +148,9 @@ def with_edge_vectors(graph: jraph.GraphsTuple, with_lengths: bool = True) -> jr
 
     edge_mask = graph.edges.get(keys.MASK)
     if edge_mask is not None:
-        edge_mask = nn_utils.prepare_mask(edge_mask, edge_vecs)
+        edge_mask = reax.metrics.utils.prepare_mask(edge_vecs, edge_mask)
         edge_vecs = jnp.where(edge_mask, edge_vecs, 1.0)
+
     if not isinstance(edge_vecs, e3j.IrrepsArray):
         edge_vecs = e3j.IrrepsArray("1o", edge_vecs)
     edges[keys.EDGE_VECTORS] = edge_vecs

src/tensorial/gcnn/_message_passing.py
@@ -6,6 +6,7 @@ from flax import linen
 import jax
 import jax.numpy as jnp
 import jaxtyping as jt
+import reax.metrics
 
 from tensorial import nn_utils, typing
 
@@ -63,13 +64,15 @@ class MessagePassingConvolution(linen.Module):
         # Get weights for the tensor product from our full-connected MLP
         if edge_mask is not None:
             radial_embedding = jnp.where(
-                nn_utils.prepare_mask(edge_mask, radial_embedding),
+                reax.metrics.utils.prepare_mask(radial_embedding, edge_mask),
                 radial_embedding,
                 0.0,
             )
         weights = mlp(radial_embedding)
         if edge_mask is not None:
-            weights = jnp.where(nn_utils.prepare_mask(edge_mask, radial_embedding), weights, 0.0)
+            weights = jnp.where(
+                reax.metrics.utils.prepare_mask(radial_embedding, edge_mask), weights, 0.0
+            )
         messages = messages * weights
 
         zeros = e3j.zeros(messages.irreps, node_feats.shape[:1], messages.dtype)

src/tensorial/gcnn/atomic.py
@@ -403,7 +403,7 @@ class EnergyContributionLstsq(reax.Metric):
         if self._type_map is None:
             num_classes = types.max().item() + 1  # Assume the types go 0,1,2...N
         else:
-            # Transform the atomic numbers into from whatever they are to 0, 1, 2....
+            # Transform the atomic numbers from whatever they are to 0, 1, 2....
             vwhere = jax.vmap(lambda num: jnp.argwhere(num == self._type_map, size=1)[0])
             types = vwhere(types)[:, 0]
             num_classes = len(self._type_map)

src/tensorial/gcnn/losses.py
@@ -10,9 +10,9 @@ import jaxtyping as jt
 import jraph
 import optax.losses
 from pytray import tree
+import reax
 
 import tensorial
-from tensorial import nn_utils
 
 from . import keys, utils
 
@@ -66,7 +66,7 @@ class Loss(GraphLoss):
         *,
         reduction: Optional[Literal["sum", "mean"]] = "mean",
         label: str = None,
-        mask_field: str = None,
+        mask_field: Optional[str] = None,
     ):
         self._loss_fn = _get_pure_loss_fn(loss_fn)
         self._prediction_field = utils.path_from_str(field)
@@ -80,7 +80,6 @@ class Loss(GraphLoss):
 
     def _call(self, predictions: jraph.GraphsTuple, targets: jraph.GraphsTuple) -> jax.Array:
         predictions_dict = predictions._asdict()
-        mask = predictions_dict[self._prediction_field[0]].get(keys.MASK)
 
         _predictions = tensorial.as_array(
             tree.get_by_path(predictions_dict, self._prediction_field)
@@ -88,30 +87,29 @@ class Loss(GraphLoss):
         _targets = tensorial.as_array(tree.get_by_path(targets._asdict(), self._target_field))
 
         loss = self._loss_fn(_predictions, _targets)
-        num_elements = loss.size
 
-        if mask is not None:
-            mask = nn_utils.prepare_mask(mask, loss)
-        # Check for the presence of a user-defined mask
+        # If there is a mask in the graph, then use it by default
+        mask = predictions_dict[self._prediction_field[0]].get(keys.MASK)
+        mask = reax.metrics.utils.prepare_mask(loss, mask)
+
+        # Now, check for the presence of a user-defined mask
         if self._mask_field:
-            user_mask = nn_utils.prepare_mask(
-                tensorial.as_array(tree.get_by_path(targets._asdict(), self._mask_field)),
-                loss,
-            )
+            user_mask = tensorial.as_array(tree.get_by_path(targets._asdict(), self._mask_field))
+            user_mask = reax.metrics.utils.prepare_mask(loss, user_mask)
             if mask is None:
                 mask = user_mask
             else:
                 mask = mask & user_mask
 
-        if mask is not None:
-            loss = jnp.where(mask, loss, 0.0)  # Zero out the masked elements
-            # Now calculate the number of elements that were masked so that we get the correct mean
-            num_elements = jnp.array([mask.sum(), *loss.shape[1:]]).prod()
+        # Now calculate the number of elements that were masked so that we get the correct mean
+        num_elements = (
+            loss.size if mask is None else jnp.array([mask.sum(), *loss.shape[1:]]).prod()
+        )
 
         if self._reduction == "mean":
-            loss = loss.sum() / num_elements
+            loss = loss.sum(where=mask) / num_elements
         elif self._reduction == "sum":
-            loss = loss.sum()
+            loss = loss.sum(where=mask)
 
         return loss
 

src/tensorial/gcnn/metrics.py
@@ -1,12 +1,12 @@
 import math
-from typing import ClassVar, Optional, TypeVar, Union
+from typing import ClassVar, Literal, Optional, TypeVar, Union
 
+import beartype
 import jax.numpy as jnp
 import jax.typing
+import jaxtyping as jt
 import jraph
 import reax
-
-from tensorial import nn_utils
 
 from . import _tree, _typing
 
@@ -15,14 +15,14 @@ OutT = TypeVar("OutT")
 __all__ = ("GraphMetric", "graph_metric")
 
 
+@jt.jaxtyped(typechecker=beartype.beartype)
 def graph_metric(
     metric: Union[str, reax.Metric, type[reax.Metric]],
     predictions: _typing.TreePathLike,
     targets: Optional[_typing.TreePathLike] = None,
-    mask: Optional[_typing.TreePathLike] = None,
+    mask: Optional[Union[_typing.TreePathLike, Literal["auto"]]] = "auto",
     normalise_by: Optional[_typing.TreePathLike] = None,
 ) -> "GraphMetric":
-
     predictions_from = _tree.path_from_str(predictions)
     targets_from = _tree.path_to_str(targets) if targets is not None else predictions_from
     mask_from = _tree.path_to_str(mask) if mask is not None else None
@@ -48,13 +48,13 @@ def mdiv(
             "Sizes of numerator and denominator must match, got {num.shape} and {denom.shape}"
         )
     if where is not None:
-        where = nn_utils.prepare_mask(where, denom)
+        where = reax.metrics.utils.prepare_mask(denom, where)
         denom = jnp.where(where, denom, 1.0)
+
     return num / denom.reshape(num.shape)
 
 
 class GraphMetric(reax.Metric):
-
     parent: ClassVar[reax.Metric]
     pred_key: ClassVar[_typing.TreePathLike]
     target_key: ClassVar[Optional[_typing.TreePathLike]] = None
@@ -92,12 +92,14 @@ class GraphMetric(reax.Metric):
         if self.mask_key is not None:
             if self.mask_key == "auto":
                 pred_key = _tree.path_from_str(self.pred_key)
-                mask_key = pred_key + ("mask",)
+                mask_key = pred_key[:-1] + ("mask",)
             else:
                 mask_key = self.mask_key
 
-            # todo: add check to this for what happens if mask doesn't exist
-            mask = _tree.get(predictions, mask_key)
+            try:
+                mask = _tree.get(predictions, mask_key)
+            except KeyError:
+                mask = None
 
         if self.normalise_by is not None:
             pred = mdiv(pred, _tree.get(predictions, self.normalise_by), where=mask)