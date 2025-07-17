9b769eedbed4f6c31b536307a2260166762c7f65
Martin Uhrin <martin.uhrin@gmail.com>
2025-07-17 16:09:11 +0200

    Added reaxkit as a package for common REAX stuff
    Contains classes and functions that can be used to easily build a
    training CLI/scripts.

---
+++ src/tensorial/reaxkit/__init__.py
@@ -0,0 +1,12 @@
+"""
+The REAX toolkit contains a bunch of classes and function that help to build a full model training
+application using tensorial and REAX.
+"""
+
+from . import config, evaluate, train
+
+__all__ = (
+    "config",
+    "evaluate",
+    "train",
+)
